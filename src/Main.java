9a9b53492188f23fbf57be47884214de286c6e16
1kovich <temirbaevkajrat132@gmail.com>
2023-10-13 19:34:23 +0600

    java 5k

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
         Random random = new Random();
         int a = random.nextInt(1,100);
         int b = random.nextInt(1,100);
+        плюс(a,b);
 
     }
+
      public static int плюс (int a, int b){
              int sum = a + b;
          System.out.println(sum);
