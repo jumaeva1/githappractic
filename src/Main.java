1138f90be65184bf4ad2b0a8d6f396b8090dadcc
Azamat966 <reallygoooool01@gmail.com>
2023-10-13 19:42:55 +0300

    Принимай

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random();
+        int a = random.nextInt(1,30);
+        int b = random.nextInt(1,30);
+        bolu(a,b);
+    }
 
-        System.out.println("kjhg");
+    static int bolu(int a, int b) {
+            System.out.println("Чон сан " + Math.max(a,b));
+            System.out.println("Кичине сан " + Math.min(b, a));
+
+
+
+
+
+        return a;
     }
-    }
+}
